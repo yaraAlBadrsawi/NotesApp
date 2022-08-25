9fd9640a52842f2b9159f6fe82d70f285e6315e3
yara sameer Al Badrsawi <59318756+yaraAlBadrsawi@users.noreply.github.com>
2022-08-25 22:37:08 +0300

    Design layouts

---
+++ app/src/main/java/com/kotlin/notesapp/data/Note.kt
@@ -0,0 +1,4 @@
+package com.kotlin.notesapp.data
+
+class Note {
+}
---
+++ app/src/main/java/com/kotlin/notesapp/views/NoteFragment.kt
@@ -0,0 +1,22 @@
+package com.kotlin.notesapp.views
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.kotlin.notesapp.R
+
+
+class NoteFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_note, container, false)
+    }
+
+
+}
